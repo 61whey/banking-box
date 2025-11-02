10856f13274c84f4a7579ca221ec11ea73cd48d6
HackAPI Team <team@hackapi.tech>
2025-11-02 23:11:21 +0300

    fix: добавлен импорт HTMLResponse для кастомного Swagger UI

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from contextlib import asynccontextmanager
 from datetime import datetime
 from pathlib import Path
